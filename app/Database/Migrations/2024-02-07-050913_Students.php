199ddf9eb814cfc67f91aa0c7680f4f3d82813c7
unknown <felipeherrerapinzon@gmail.com>
2024-02-07 00:23:27 -0500

    updated migration

---
+++ app/Database/Migrations/2024-02-07-050913_Students.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Students extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'first_name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'last_name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'address' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ]
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('students');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('students');
+    }
+}
